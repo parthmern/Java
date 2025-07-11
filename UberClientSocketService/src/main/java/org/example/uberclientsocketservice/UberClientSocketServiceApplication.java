f9c753bf37bc924180f5463f6e25d61dcb56e3ac
parthmern <parthmern@gmail.com>
2025-07-10 22:21:40 -0400

    intiated websocket service

---
+++ UberClientSocketService/src/main/java/org/example/uberclientsocketservice/UberClientSocketServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.uberclientsocketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberClientSocketServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberClientSocketServiceApplication.class, args);
+    }
+
+}
---
+++ UberClientSocketService/src/test/java/org/example/uberclientsocketservice/UberClientSocketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.uberclientsocketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberClientSocketServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
